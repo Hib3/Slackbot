9036f7aaacd65804ff05097baf575b88666f5eaa
ﾋﾋﾞｷ <34598602+Hib3@users.noreply.github.com>
2018-12-07 08:45:29 +0900

    Update slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-465703587492-485956620084-RbilJjC9xifqbSsM8GefrWQA"
+API_TOKEN = ""
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Now I can only respond to the weather\nPlz enter weather"

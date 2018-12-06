4355c08ad915d947feac6d991813f290712c7a7b
hibiki <hbk@scarecrow.local>
2018-12-06 23:59:19 +0900

    test

---
+++ plugins/my_mention.py
@@ -0,0 +1,8 @@
+from slackbot.bot import respond_to     # @botname: で反応するデコーダ
+from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
+from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+
+@respond_to('夢占い')
+def mention_func(message):
+    message.reply(‘hi how are u’) # メンション
+    import scraping.py
---
+++ plugins/scraping.py
@@ -0,0 +1,27 @@
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+"""オプション設定
+options = Options()
+options.add_argument('--headless')#ヘッドレスモードにより、裏で動かす
+options.add_argument('--disable-gpu')#GPU使うようなページではオフにさせる
+driver = webdriver.Chrome(options=options)
+"""
+driver = webdriver.Chrome("./chromedriver")
+
+url = 'http://yume-uranai.jp/keyword.html'
+words = ('牛 カエル')
+
+driver.get(url)
+sleep(1)
+
+element = driver.find_element_by_name("keyword")
+element.send_keys(words)
+element.submit()#入力formの送信
+sleep(1)
+
+sleep(5)
+
+driver.close()
